9466a2abc6b82e7e59040e472ffed56da2f84544
Chris Curnow <chris@4cast.com.au>
2022-01-06 19:24:39 +1100

    initial commit

---
+++ apps/hoidays/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'holidays-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hoidays';
+}
